47a5f58951cd860cd187117105b32c571a961708
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-21 22:00:21 +0800

    added react-linkify which parse urls in strings to links

--- TINA2.0/src/components/Response/Response.tsx
+++ TINA2.0/src/components/Response/Response.tsx
@@ -6,6 +6,8 @@ import {
 } from "react-icons/md";
 import { useSelector } from "react-redux";
 import TINA from "../../assets/avatars/tinaAvatarTransparent.png";
+import Linkify from "react-linkify";
+import ReactLinkify from "react-linkify";
 
 type Props = {
   response: string;
@@ -25,6 +27,15 @@ const parseStringToLink = (input: string) => {
 function Response({ response, error }: Props) {
   const parsedResponse = parseStringToLink(response);
   const loading = response === "";
+
+  const properties = {
+    target: "_blank",
+    style: {
+      color: "blue",
+      fontWeight: "bold",
+    },
+  };
+
   return (
     <div className="flex w-full snap-start items-center justify-center bg-gold drop-shadow-lg dark:bg-darkSoft">
       <div
@@ -32,31 +43,46 @@ function Response({ response, error }: Props) {
           loading ? "gap-4" : "gap-6"
         } whitespace-pre-wrap pt-6 pb-10 text-base md:text-sm`}
       >
-        <div className="relative flex h-8 w-8 justify-start">
-          <img src={TINA} alt="" className="h-8 w-8 -scale-x-100" />
-          {error && (
+        {error && (
+          <div className="relative flex h-8 w-8 justify-start">
+            <img src={TINA} alt="" className="h-8 w-8 -scale-x-100" />
             <MdErrorOutline
               className="absolute -bottom-0.5 -right-1 rounded-full bg-red"
               size={"1.1em"}
             />
-          )}
-        </div>
+          </div>
+        )}
+
+        {!error && <img src={TINA} alt="" className="h-8 w-8 -scale-x-100" />}
 
         {loading ? (
           <span className="inline-block h-4 w-3 animate-[cursor-blink_1.25s_steps(2)_infinite] bg-darkBgLighter dark:bg-divider"></span>
         ) : (
-          <div
-            className={`
-          prose break-words rounded-md bg-[#f6e5e6] p-2 pl-3 text-[15px]  outline outline-1  dark:bg-darkTextSoft  
+          <Linkify
+            componentDecorator={(decoratedHref, decoratedText, key) => (
+              <a
+                target={"_blank"}
+                href={decoratedHref}
+                key={key}
+                className="font-medium text-blueBorder hover:font-semibold dark:text-darkBlueSoft"
+              >
+                {decoratedText}
+              </a>
+            )}
+          >
+            <div
+              className={`
+          prose break-words rounded-md rounded-tl-none bg-[#f6e5e6] p-2 px-3 text-[15px] outline dark:bg-darkTextSoft  
           ${
             error
-              ? "text-redLight outline-red"
-              : "text-darkBgLight outline-darkDivider dark:text-text dark:outline-darkBgLighter"
+              ? "text-redLight outline-2 outline-red"
+              : "text-darkBgLight outline-1 outline-darkDivider dark:text-text dark:outline-darkBgLighter"
           }
           `}
-          >
-            {response}
-          </div>
+            >
+              {response}
+            </div>
+          </Linkify>
         )}
       </div>
     </div>
