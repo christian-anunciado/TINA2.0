9b18aecb0c1019a8596de99ffbd2c44c7c164354
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-22 14:03:36 +0800

    added responsive for query text

--- TINA2.0/src/components/Responses/Responses.tsx
+++ TINA2.0/src/components/Responses/Responses.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { MessageState, MessageType } from "../../redux/convoRedux";
 import Query from "../Query/Query";
 import Response from "../Response/Response";
+import ResponseCopy from "../Response/ResponseCopy";
 
 interface Message {
   text: string;
@@ -25,6 +26,27 @@ function Responses({ messages }: Props) {
     ref.current?.scrollTo(0, ref.current.scrollHeight);
   }, [conversation.messages]);
 
+  const test = `New/Returnee/Transfer requirements:
+  1. Report Card w/LRN (Learner's Reference Number)
+  2. Certificate of Good Moral Character
+  3. Medical Certificate - waived
+  4. PSA-Authenticated Birth Certificate
+  5. Parent/Guardian's Valid ID
+  
+  Old/Continuing:
+  1. Pupil Report Card
+  2. Parent/Guardian's Valid ID
+  3. Pupil's CIT School ID`;
+
+  const test2 = `Yes, we accept transferees. These are the following requirements: 
+  1. Report Card / Form 138 with LRN (Kinder to Grade 6) (Required) 
+  2. PSA-Authenticated Birth Certificate 
+  3. Medical Certificate - waived 
+  4. Certificate of Good Moral Character (Grades 1-6) 
+  5. Parent/Guardian's Valid ID Enroll with NO ENROLMENT FEE REQUIRED (payment is deferred until further notice) through this link in our website: https://cit.edu/quick-links/`;
+
+  const test3 = `The requirements for admission to CIT may vary depending on the degree program and level you are applying for. Generally, you will need to provide your academic records, a completed application form, and any other supporting documents as required by the program. You can check the CIT website or contact ETO for more information about specific requirements.`;
+
   return (
     <div
       className="flex min-h-0 flex-[4] snap-y flex-col items-center gap-1 overflow-auto scroll-smooth"
@@ -42,6 +64,7 @@ function Responses({ messages }: Props) {
             />
           );
       })}
+      <Response response={test} />
     </div>
   );
 }
