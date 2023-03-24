91cb6153eeb13c86fba036d811a626b7f93bba25
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-25 01:54:32 +0800

    added production url in axios

--- TINA2.0/src/api/response.tsx
+++ TINA2.0/src/api/response.tsx
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const env = process.env.NODE_ENV;
+
+const url =
+  env === "development"
+    ? "http://127.0.0.1:8000/"
+    : "https://tina20server-production.up.railway.app/";
+
 const generate_response = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: url,
 });
 
 export default generate_response;
