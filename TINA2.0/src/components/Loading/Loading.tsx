0c7f5bd12cc4e9a31066d1b96d0c888303123999
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-04-02 22:49:09 +0800

    changed color back to original gold

--- TINA2.0/src/components/Loading/Loading.tsx
+++ TINA2.0/src/components/Loading/Loading.tsx
@@ -7,7 +7,7 @@ function Loading({}: Props) {
   const darkMode = useSelector((state: any) => state.theme.darkMode);
   return (
     <div className={`${darkMode && "dark"}`}>
-      <div className="flex h-[100svh] flex-col items-center justify-center gap-3 bg-primaryGold dark:bg-darkBgLight lg:gap-4">
+      <div className="flex h-[100svh] flex-col items-center justify-center gap-3 bg-gold dark:bg-darkBgLight lg:gap-4">
         <img
           src={CITLogo}
           className="h-20 w-20 animate-pulse md:h-24 md:w-24 lg:h-28 lg:w-28"
--- TINA2.0/src/components/Response/Response.tsx
+++ TINA2.0/src/components/Response/Response.tsx
@@ -19,7 +19,7 @@ function Response({ response, error }: Props) {
   const loading = response === "";
 
   return (
-    <div className="flex w-full items-center justify-center bg-primaryGold drop-shadow-lg dark:bg-darkSoft">
+    <div className="flex w-full items-center justify-center bg-gold drop-shadow-lg dark:bg-darkSoft">
       <div
         className={`group flex w-[80%] md:w-[60%] xl:w-[48%] ${
           loading ? "items-center" : null
