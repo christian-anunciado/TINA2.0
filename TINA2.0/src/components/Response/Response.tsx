f094f6903381e17739f6889ff2ec8c8e0a54ac36
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-04-02 15:40:49 +0800

    change colors the same as CIT branding

--- TINA2.0/src/components/Response/Response.tsx
+++ TINA2.0/src/components/Response/Response.tsx
@@ -19,7 +19,7 @@ function Response({ response, error }: Props) {
   const loading = response === "";
 
   return (
-    <div className="flex w-full items-center justify-center bg-gold drop-shadow-lg dark:bg-darkSoft">
+    <div className="flex w-full items-center justify-center bg-primaryGold drop-shadow-lg dark:bg-darkSoft">
       <div
         className={`group flex w-[80%] md:w-[60%] xl:w-[48%] ${
           loading ? "items-center" : null
@@ -32,7 +32,7 @@ function Response({ response, error }: Props) {
             <img src={TINA} alt="" className="h-8 w-8 -scale-x-100" />
             <MdErrorOutline
               className="absolute -bottom-0.5 -right-1 rounded-full bg-red"
-              size={"1.1em"}
+              size={"1em"}
             />
           </div>
         )}
@@ -48,7 +48,7 @@ function Response({ response, error }: Props) {
           }
           ${
             !loading &&
-            "bg-[#f6e5e6] p-2.5 pl-3 pr-2 pb-3.5 outline dark:bg-darkTextSoft"
+            "bg-[#f6e5e6] p-2.5 pl-3 pr-3 pb-3.5 outline dark:bg-darkTextSoft md:text-justify"
           }
           `}
         >
